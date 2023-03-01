ce2f4fa2b184498f2e965d24c2429e3ce76e2547
Iqbal Tahir <iqbalknightphantom33@gmail.com>
2023-03-02 00:10:50 +0800

    first commit

--- application/views/_frontend/page/news_detail.php
+++ application/views/_frontend/page/news_detail.php
@@ -36,119 +36,19 @@
                                         </p>
 
                                     </div>
-
-
-
-                                    <div class="entry-content">
-
-                                        <?php if ($n->news_category_id == 2) { ?>
-
-                                            <div class="read-more" style="text-align-last:center">
-
-                                                <a href="<?php echo $n->news_link; ?>">Visit Project</a>
-
-                                            </div>
-
-                                        <?php } ?>
-
-
-
-                                    </div>
-
                                 </article>
-
+                                <div class="entry-content">
+                                    <?php if (!$n->news_link == '') { ?>
+                                        <div class="read-more">
+                                            <a class="button" href="<?php echo $n->news_link ?>" target="_blank"><i class="icofont-link"></i> &nbsp;Visit Project</a>
+                                        </div>
+                                    <?php } ?>
+                                </div>
                             <?php } ?>
-
                         </div>
-
-
-
-                        <!--<div class="col-lg-4">-->
-
-
-
-                        <!--<div class="sidebar">-->
-
-                        <!--        <h3 class="color-1 text-2">Pencarian <?php echo $news_category_name[0]->news_category_name; ?></h3>-->
-
-                        <!--        <hr style="border: 0.9px dashed #d2d6de">-->
-
-                        <!--        <div class="sidebar-item search-form">-->
-
-                        <!--            <?php echo form_open("page/information_search/" . $this->uri->segment(3) . "/" . $this->uri->segment(4)) ?>-->
-
-                        <!--                <input type="text" name="key" placeholder="Cari Judul">-->
-
-                        <!--                <button type="submit" style="background-color:#FFCE31; color:#4d4d4d">Cari</button>-->
-
-                        <!--            <?php echo form_close(); ?>-->
-
-                        <!--        </div>-->
-
-
-
-                        <!--        <h3 class="color-1 text-2">Kategori</h3>-->
-
-                        <!--        <hr style="border: 0.9px dashed #d2d6de">-->
-
-                        <!--        <div class="sidebar-item categories">-->
-
-                        <!--            <ul>-->
-
-                        <!--                <?php foreach ($news_category as $nc) { ?>-->
-
-                        <!--                    <li><a href="<?php echo site_url('page/information/' . $nc->news_category_id . '/' . $this->uri->segment(4)); ?>"><?php echo $nc->news_category_name; ?></a></li>-->
-
-                        <!--                <?php } ?>-->
-
-                        <!--            </ul>-->
-
-                        <!--        </div>-->
-
-
-
-
-
-                        <!--        <h3 class="color-1 text-2">Artikel Terbaru</h3>-->
-
-                        <!--        <hr style="border: 0.9px dashed #d2d6de">-->
-
-                        <!--        <div class="sidebar-item recent-posts">-->
-
-                        <!--            <?php foreach ($recent as $r) { ?>-->
-
-                        <!--            <div class="post-item clearfix">-->
-
-                        <!--                <img src="<?php echo base_url(); ?>upload/news/<?php echo $r->news_cover ?>" alt="">-->
-
-                        <!--                <h4><a href="<?php echo site_url('page/information_detail/' . $r->news_category_id . '/' . $r->field_id . '/' . $r->news_slug); ?>"><?php echo $r->news_title; ?></a></h4>-->
-
-                        <!--                <time style="color:gray"><i class="icofont-eye-alt"></i> <b><?php echo $r->news_count_view; ?>x dilihat</b> &nbsp;&nbsp;<b class="badge badge-success"><?php echo $r->field_name; ?></b></time>-->
-
-                        <!--            </div>-->
-
-                        <!--            <?php } ?>-->
-
-                        <!--        </div>-->
-
-                        <!--    </div>-->
-
-
-
-                        <!--</div>-->
-
-
-
                     </div>
-
-
-
                 </div>
-
             </section>
-
         </div>
-
     </section>
-
 </main>
