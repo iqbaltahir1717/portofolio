78fca272cc8d13087ae688f847e1a1578701bc40
Iqbal Tahir <iqbalknightphantom33@gmail.com>
2023-03-02 00:13:42 +0800

    firts commit

--- application/views/_frontend/page/news_detail.php
+++ application/views/_frontend/page/news_detail.php
@@ -10,31 +10,22 @@
 <main id="main">
     <section id="services" class="services">
         <div class="container" data-aos="fade-up">
-            <section id="blog" class="blog">
+            <section id="blog" class="blog" style="padding:0">
                 <div class="container" data-aos="fade-up">
                     <div class="row">
                         <div class="col-lg-12 entries-2">
                             <?php foreach ($news as $n) { ?>
                                 <article class="entry-3">
                                     <div class="entry-img">
-                                        <img src="<?php echo base_url(); ?>upload/news/<?php echo $n->news_cover; ?>" alt="" class="img-fluid img-thumbnail">
+                                        <img src="<?php echo base_url(); ?>upload/news/<?php echo $n->news_cover; ?>" alt="" class="img-fluid img-thumbnail" sty>
                                     </div>
-                                    <br>
-
                                     <div class="entry-value">
-
                                         <h2 class="color-1 text-1 weight-600">
-
                                             <p href=""><?php echo $n->news_title; ?></p>
-
                                         </h2>
-
                                         <p>
-
                                             <?php echo $n->news_text; ?>
-
                                         </p>
-
                                     </div>
                                 </article>
                                 <div class="entry-content">
