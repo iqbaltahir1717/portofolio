716ed60b02a5551792dd14fbc6841ab0e12f8fe6
Iqbal Tahir <iqbalknightphantom33@gmail.com>
2023-03-05 13:50:24 +0800

    first commit

--- application/views/_frontend/page/news_detail.php
+++ application/views/_frontend/page/news_detail.php
@@ -8,26 +8,17 @@
 </section>
 
 <main id="main">
-    <section id="services" class="services">
+    <section id="services" class="services" style="padding:20px 0">
         <div class="container" data-aos="fade-up">
             <section id="blog" class="blog" style="padding:0">
-                <div data-aos="fade-up">
+                <div>
                     <div class="row">
                         <div class="col-lg-12 entries-2">
+                            <a href="<?php echo site_url('page/index/1/1') ?>"><i class="icofont-arrow-left"></i> See All Projects</a>
                             <?php foreach ($news as $n) { ?>
-                                <article class="entry-3">
-                                    <div class="entry-img">
-                                        <img src="<?php echo base_url(); ?>upload/news/<?php echo $n->news_cover; ?>" alt="" class="img-fluid img-thumbnail" sty>
-                                    </div>
-                                    <div class="entry-value">
-                                        <h2 class="color-1 text-1 weight-600">
-                                            <p href=""><?php echo $n->news_title; ?></p>
-                                        </h2>
-                                        <p>
-                                            <?php echo $n->news_text; ?>
-                                        </p>
-                                    </div>
-                                </article>
+                                <h2 class="color-1 text-1 weight-600 my-5">
+                                    <p href=""><?php echo $n->news_title; ?></p>
+                                </h2>
                                 <div class="entry-content">
                                     <div class="read-more">
                                         <?php if (!$n->news_link == '') { ?>
@@ -38,6 +29,17 @@
                                         <?php } ?>
                                     </div>
                                 </div>
+                                <article class="entry-3">
+                                    <div class="entry-img">
+                                        <img src="<?php echo base_url(); ?>upload/news/<?php echo $n->news_cover; ?>" alt="" class="img-fluid glightbox img-thumbnail" sty>
+                                    </div>
+                                    <div class="entry-value">
+                                        <p>
+                                            <?php echo $n->news_text; ?>
+                                        </p>
+                                    </div>
+                                </article>
+
                             <?php } ?>
                         </div>
                     </div>
